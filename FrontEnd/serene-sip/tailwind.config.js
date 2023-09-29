8fa623319b3ba4f159cee481721ef61d2ce2f706
NicholasVB <tivalik2@gmail.com>
2023-09-29 11:00:49 +0200

    additional configuring tailwind files

--- FrontEnd/serene-sip/tailwind.config.js
+++ FrontEnd/serene-sip/tailwind.config.js
@@ -27,6 +27,39 @@ module.exports = {
         'white': '#FFFFFF',
         'black': '#000000',
       },
+      screens: {
+        'xxs': '320px',
+        // => @media (min-width: 320px) { ... }
+
+        'xls': '375px',
+        // => @media (min-width: 375px) { ... }
+
+        'xs': '480px',
+        // => @media (min-width: 480px) { ... }
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        'mdl': '992px',
+        // => @media (min-width: 992px) { ... }
+
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px'
+      },
+      gap: {
+        '15': '3.75rem',
+      },
+      spacing: {
+        '15': '3.75rem',
+      }
     },
   },
   plugins: [],
