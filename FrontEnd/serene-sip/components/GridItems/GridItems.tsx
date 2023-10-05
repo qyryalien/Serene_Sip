88b42c04ed608577c79ee3d00f070adc11ae0d34
NicholasVB <tivalik2@gmail.com>
2023-10-05 09:17:58 +0300

    Added a key property to the ProductCard component when rendering an array in the GridItems component.

--- FrontEnd/serene-sip/components/GridItems/GridItems.tsx
+++ FrontEnd/serene-sip/components/GridItems/GridItems.tsx
@@ -5,7 +5,7 @@ import { ProductCard } from "../ProductCard/ProductCard";
 export function GridItems() {
     return (
         <div className='grid xxs:grid-rows-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-full'>
-            {data.products.map((product, index) => <ProductCard product={product} index={index}></ProductCard>)}
+            {data.products.map((product, index) => <ProductCard key={product.id} product={product} index={index}></ProductCard>)}
         </div>
     )
 }
